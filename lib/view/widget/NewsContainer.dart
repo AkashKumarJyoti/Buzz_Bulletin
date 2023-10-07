b11cee804e47e2962ab0c5f4b2985b64fe2321b4
Akash Kumar Jyoti <akashjyoti2002@gmail.com>
2023-10-07 13:54:54 +0530

    responsive

--- lib/view/widget/NewsContainer.dart
+++ lib/view/widget/NewsContainer.dart
@@ -37,6 +37,7 @@ class _NewsContainerState extends State<NewsContainer> {
 
   @override
   Widget build(BuildContext context) {
+    var screenheight = MediaQuery.of(context).size.height;
     return SizedBox(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
@@ -45,37 +46,47 @@ class _NewsContainerState extends State<NewsContainer> {
           children: <Widget>[
 
             // News Image
-            widget.imgUrl == 'images/breaking_news.jpg'     // If condition true then show this image
-                ? Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 2.0),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(10),
-                      child: Image.asset(
-                        'images/breaking_news.jpg',
-                        height: 200,
-                        width: MediaQuery.of(context).size.width,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  )
-                : Padding(                                // Show FadeInImage
-                    padding: const EdgeInsets.symmetric(horizontal: 2.0),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(10),
-                      child: FadeInImage(
-                        height: 200,
-                        width: MediaQuery.of(context).size.width,
-                        fit: BoxFit.cover,
-                        placeholder: const AssetImage('images/breaking_news.jpg'),
-                        image: NetworkImage(widget.imgUrl),
-                        imageErrorBuilder: (context, error, stackTrace)
-                        {
-                          return Image.asset('images/breaking_news.jpg');        // It will handle if the url image does not exist
-                        },
-                      ),
-                    ),
+            Visibility(
+              visible: screenheight >= 200,
+              child: widget.imgUrl == 'images/breaking_news.jpg'     // If condition true then show this image
+                  ? Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 2.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.asset(
+                    'images/breaking_news.jpg',
+                    height: MediaQuery.of(context).size.height/3,
+                    width: MediaQuery.of(context).size.width,
+                    fit: BoxFit.cover,
                   ),
-            const SizedBox(height: 20.0),
+                ),
+              )
+                  : Padding(                                // Show FadeInImage
+                padding: const EdgeInsets.symmetric(horizontal: 2.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: FadeInImage(
+                    height: MediaQuery.of(context).size.height/3,
+                    width: MediaQuery.of(context).size.width,
+                    fit: BoxFit.cover,
+                    placeholder: const AssetImage('images/breaking_news.jpg'),
+                    image: NetworkImage(widget.imgUrl),
+                    imageErrorBuilder: (context, error, stackTrace)  // It will handle if the url image does not exist
+                    {
+                      return Image.asset(
+                        'images/breaking_news.jpg',
+                        height: MediaQuery.of(context).size.height/3,
+                        width: MediaQuery.of(context).size.width,
+                        fit: BoxFit.cover,
+                      );
+                    },
+                  ),
+                ),
+              )
+            ),
+            Visibility(
+                visible: screenheight >= 260,
+                child: const SizedBox(height: 20.0)),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 8.0),
               child: Column(
@@ -83,42 +94,60 @@ class _NewsContainerState extends State<NewsContainer> {
                 children: <Widget>[
 
                   // News published date
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      const Text("Published at -: "),
-                      Text(widget.time,
-                          style: const TextStyle(color: Colors.deepPurple))
-                    ],
+                  Visibility(
+                    visible: screenheight >= 280,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        const Text("Published at -: "),
+                        Text(widget.time,
+                            style: const TextStyle(color: Colors.deepPurple))
+                      ],
+                    ),
                   ),
-                  const SizedBox(height: 4.0),
+                  Visibility(
+                      visible: screenheight >= 260,
+                      child: const SizedBox(height: 4.0)),
 
                   // Headline
-                  HtmlWidget(
-                      // Using HtmlWidget instead of Text because some data may contain in the form of html
-                      widget.newsHead.length > 70
-                          ? "${widget.newsHead.substring(0, 70)}...."
-                          : widget.newsHead,
-                      textStyle: const TextStyle(
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold,
-                      )),
-                  const SizedBox(height: 10.0),
+                  Visibility(
+                    visible: screenheight >= 450,
+                    child: HtmlWidget(
+                        // Using HtmlWidget instead of Text because some data may contain in the form of html
+                        widget.newsHead.length > 70
+                            ? "${widget.newsHead.substring(0, 70)}...."
+                            : widget.newsHead,
+                        textStyle: TextStyle(
+                          fontSize: screenheight/30,
+                          fontWeight: FontWeight.bold,
+                        )),
+                  ),
+                  Visibility(
+                      visible: screenheight >= 250,
+                      child: const SizedBox(height: 10.0)),
 
                   // News Description
-                  HtmlWidget(widget.newsDesc,
-                      textStyle:
-                          const TextStyle(fontSize: 12, color: Colors.black38)),
-                  const SizedBox(height: 10.0),
+                  Visibility(
+                    visible: screenheight >= 455,
+                    child: HtmlWidget(widget.newsDesc,
+                        textStyle:
+                            TextStyle(fontSize: screenheight/60, color: Colors.black38)),
+                  ),
+                  Visibility(
+                      visible: screenheight >= 250,
+                      child: const SizedBox(height: 10.0)),
 
                   // News Content
-                  HtmlWidget(
-                      widget.newsContent != "--"
-                          ? widget.newsContent.length > 250
-                              ? "${widget.newsContent.substring(0, 250)}...."
-                              : "${widget.newsContent.substring(0, widget.newsContent.length - 15)}...."
-                          : widget.newsContent,
-                      textStyle: const TextStyle(fontSize: 16.0))
+                  Visibility(
+                    visible: screenheight >= 455,
+                    child: HtmlWidget(
+                        widget.newsContent != "--"
+                            ? widget.newsContent.length > 250
+                                ? "${widget.newsContent.substring(0, 250)}...."
+                                : "${widget.newsContent.substring(0, widget.newsContent.length - 15)}...."
+                            : widget.newsContent,
+                        textStyle: TextStyle(fontSize: screenheight/45)),
+                  )
                 ],
               ),
             ),
@@ -142,10 +171,15 @@ class _NewsContainerState extends State<NewsContainer> {
                 ),
               ],
             ),
-            const SizedBox(
-              height: 10.0,
+            Visibility(
+              visible: screenheight >= 270,
+              child: const SizedBox(
+                height: 10.0,
+              ),
             ),
-            const Center(child: Icon(Icons.keyboard_double_arrow_down_outlined, color: Colors.deepPurple)),
+            Visibility(
+              visible: screenheight >= 310,
+              child: const Center(child: Icon(Icons.keyboard_double_arrow_down_outlined, color: Colors.deepPurple))),
             const SizedBox(height: 10.0)
           ],
         ));
